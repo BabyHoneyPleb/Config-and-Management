4fc936a558a31ce484156dea2cc44014ccd0ea9c
Austin Danaj <afdanaj@oakland.edu>
2017-01-25 20:34:49 -0500

    Create BeerSong.cs
    Created beersong class, added empty functions, one for main, the other for the song

---
+++ BeerSong.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Beersong
+{
+    class BeerSong
+    {
+        public BeerSong()
+        {
+
+        }
+        public void Ninety_Nine_Bottles_of_Beer()
+        {
+        
+        }
+    }
+}
